14ed5bae53993363bc9918796b7e2b1710a21f87
Anders Hedstrom <anders@alhem.net>
2025-08-20 16:21:31 +0200

    remove unused script

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p dist/bin/
-wget https://www.alhem.net/tmp/artify -O dist/bin/artify
-export PATH=$PATH:`pwd`/dist/bin
--- src/create-dist.sh
+++ src/create-dist.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+DEST=../dist
+rm -rf $DEST
+mkdir -p $DEST/bin/
+wget https://www.alhem.net/tmp/artify -O $DEST/bin/artify
+export PATH=$PATH:`pwd`/dist/bin
 artify build Sockets.fc
 unzip -d .. -o -q Sockets.zip
 rm -f Sockets.zip Sockets.json
