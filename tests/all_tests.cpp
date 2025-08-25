c49ddbca8c7532f81eea1ecf2c82017bf2911302
Anders Hedström <97519292+lorangriel@users.noreply.github.com>
2025-08-25 14:06:20 +0200

    Print test names during CppUnit runs

--- tests/all_tests.cpp
+++ tests/all_tests.cpp
@@ -5,6 +5,17 @@
 #include "http_server_test.h"
 #include "base64_tests.h"
 #include "utility_tests.h"
+#include <cppunit/TestListener.h>
+#include <cppunit/Test.h>
+#include <cppunit/TestResult.h>
+#include <iostream>
+
+class VerboseTestProgressListener : public CppUnit::TestListener {
+public:
+    void startTest(CppUnit::Test* test) override {
+        std::cout << "Running: " << test->getName() << std::endl;
+    }
+};
 
 int main(int , char* argv[])
 {
@@ -12,7 +23,9 @@ int main(int , char* argv[])
     std::filesystem::current_path(exe_path.parent_path());
 
     CppUnit::TextUi::TestRunner runner;
+    VerboseTestProgressListener progress;
+    runner.eventManager().addListener(&progress);
     runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
-    return runner.run() ? 0 : 1;
+    return runner.run("", false, true, false) ? 0 : 1;
 }
 
